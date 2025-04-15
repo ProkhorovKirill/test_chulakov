025c70ea1038c723da57a933783337b79d890478
ProkhorovKirill <Prokhorov187@yandex.ru>
2025-04-15 22:03:23 +0300

    add third section(html, css, js)

--- test_project/src/main.js
+++ test_project/src/main.js
@@ -1,3 +1,34 @@
 import './css/style.css'
 import './css/sanitize.css'
 //import viteLogo from '/vite.svg'
+
+const phoneInput = document.getElementById('phone_number');
+
+phoneInput.addEventListener('focus', (e) => {
+    e.target.value = '+7'
+})
+
+phoneInput.addEventListener('input', (e) => {
+    let value = e.target.value.replace(/\D/g, '');
+    if (value.startsWith('7')) {
+        value = value.substring(1);
+    }
+    if (value.length > 10) {
+        value = value.substring(0, 10);
+    }
+
+    let formatted = '+7';
+    if (value.length > 0) {
+        formatted += ` (${value.substring(0,3)}`;
+    }
+    if (value.length > 3) {
+        formatted += `) ${value.substring(3,6)}`;
+    }
+    if (value.length > 6) {
+        formatted += `-${value.substring(6,8)}`;
+    }
+    if (value.length > 8) {
+        formatted += `-${value.substring(8,10)}`;
+    }
+    e.target.value = formatted;
+});
