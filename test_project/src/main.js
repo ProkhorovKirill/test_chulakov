462b31e128e91ffdf9dd2af24a5eca15c05439df
ProkhorovKirill <Prokhorov187@yandex.ru>
2025-04-17 15:30:24 +0300

    add mobile adaptive

--- test_project/src/main.js
+++ test_project/src/main.js
@@ -3,8 +3,18 @@ import './css/sanitize.css'
 
 const dialog = document.querySelector('.dialog');
 const city = document.querySelector('.header_location_city');
+const template = document.querySelector('.template');
+const changeCity = document.querySelector('.header_modal_location_change');
+const chooseCity = document.querySelector('.choose_city');
 
-dialog.show()
+
+
+
+if (!localStorage.getItem('city')){
+    dialog.show();
+} else {
+    city.textContent = localStorage.getItem('city');
+}
 
 const closeModal = document.querySelector('.header_modal_location_true');
 
@@ -13,29 +23,26 @@ closeModal.addEventListener('click', () => {
     localStorage.setItem('city', city.textContent)
 });
 
-const locationQuestion = document.querySelector('.header_modal_location_question')
-
-if (localStorage.getItem('city')){
-    locationQuestion.textContent = `Ваш регион ${localStorage.getItem('city')}?`
-}
-
-const template = document.querySelector('.template');
-const changeCity = document.querySelector('.header_modal_location_change');
-const chooseCity = document.querySelector('.choose_city');
-
 chooseCity.appendChild(template.content.cloneNode(true));
 
 changeCity.addEventListener('click', () => {
-    chooseCity.showModal()
+    chooseCity.showModal();
+    dialog.close()
 })
-
-console.log(city.textContent);
-
-chooseCity.onclick = () => {
-    chooseCity.close()
+chooseCity.onclick = (e) => {
+    if (!e.target.closest('li')) return;
+    localStorage.setItem('city', e.target.textContent);
+    chooseCity.close();
+    setTimeout(() => {
+        city.textContent = localStorage.getItem('city');
+    }, 0);
 }
 
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+const headerLocation = document.querySelector('.header_location');
+
+headerLocation.addEventListener('click', () => {
+    chooseCity.showModal();
+})
 
 const phoneInput = document.getElementById('phone_number');
 
@@ -73,4 +80,22 @@ document.querySelector('.up_button').addEventListener('click', () => {
         top: 0,
         behavior: 'smooth'
     });
+})
+
+const phoneNumber = document.querySelector('.phone_number_input');
+const form = document.querySelector('.get_promocode');
+const failedQuery = document.querySelector('.promocode_failed');
+const successQuery = document.querySelector('.promocode_success');
+const formSuccess = document.querySelector('.form_success');
+
+form.addEventListener('submit', (e) => {
+    if(localStorage.getItem('phoneNumber') == phoneNumber.value){
+        e.preventDefault();
+        failedQuery.classList.remove('promocode_failed--hidden');
+    } else {
+        e.preventDefault()
+        localStorage.setItem('phoneNumber', phoneNumber.value);
+        successQuery.classList.remove('promocode_success--hidden');
+        formSuccess.classList.remove('form_success--hidden')
+    }
 })
